99b5b9d02a6c1835db928620118d41ad68a7e998
Conchylicultor <etiennefg.pot@gmail.com>
2016-04-12 10:57:16 +0200

    Add base script

---
+++ main.py
@@ -0,0 +1,32 @@
+ """
+Main script
+
+Use python 3
+"""
+
+import os, sys
+import numpy as np
+
+# Parametters
+nbEpoch = 150
+
+def main():
+    # Loading dataset
+    
+    # Datatransform (normalisation, remove outliers,...)
+    
+    # TODO: Include the training in the cross-validation loop
+    # Main loop
+    for i in range(nbEpoch):
+        print("Epoch: ", i)
+        # Forward pass
+        
+        # Backward pass
+        
+        # Update the weights
+        
+        # Saving the model
+
+
+if __name__ == "__main__":
+    main()
